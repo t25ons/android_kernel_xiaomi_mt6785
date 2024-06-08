393789210d37e489548dcfaf14e3a7f49367ce35
Wahid Khan <wahidzk0091@gmail.com>
2024-06-08 10:47:40 +0530

    drivers/misc: eccci: guard traffic_monitor with TRACING
    Signed-off-by: Wahid Khan <wahidzk0091@gmail.com>

--- drivers/misc/mediatek/eccci/hif/ccci_hif_dpmaif.c
+++ drivers/misc/mediatek/eccci/hif/ccci_hif_dpmaif.c
@@ -4438,7 +4438,9 @@ int dpmaif_stop(unsigned char hif_id)
 	/* rx rx clear */
 	dpmaif_stop_rx_sw(hif_id);
 	/* stop debug mechnism */
+#if DPMAIF_TRAFFIC_MONITOR_INTERVAL
 	del_timer(&dpmaif_ctrl->traffic_monitor);
+#endif
 
 	/* CG set */
 	ccci_hif_dpmaif_set_clk(0);
