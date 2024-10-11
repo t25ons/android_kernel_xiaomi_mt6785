b792d0e57c6037f6803e6ce8671d48872ee41e90
fukiame <fukiame@proton.me>
2024-10-11 11:48:08 +0700

    drivers/misc: mediatek: video: remove all MTK_GMO_RAM_OPTIMIZE guard
    this disables the ridiculous "display recorder" thing mtk has.
    
    Signed-off-by: fukiame <fukiame@proton.me>

--- drivers/misc/mediatek/video/mt6785/dispsys/display_recorder.c
+++ drivers/misc/mediatek/video/mt6785/dispsys/display_recorder.c
@@ -47,7 +47,7 @@
 #include "ddp_mmp.h"
 #include <linux/trace_events.h>
 
-#if defined(CONFIG_MTK_ENG_BUILD) || !defined(CONFIG_MTK_GMO_RAM_OPTIMIZE)
+#if defined(CONFIG_MTK_ENG_BUILD)
 unsigned int gCapturePriLayerEnable;
 unsigned int gCaptureWdmaLayerEnable;
 unsigned int gCaptureRdmaLayerEnable;
@@ -1204,7 +1204,7 @@ char *debug_buffer;
 
 int debug_buffer_size(void)
 {
-#if defined(CONFIG_MT_ENG_BUILD) || !defined(CONFIG_MTK_GMO_RAM_OPTIMIZE)
+#if defined(CONFIG_MTK_ENG_BUILD)
 	static int size;
 
 	if (size == 0) {
@@ -1384,7 +1384,7 @@ int dprec_logger_get_buf(enum DPREC_LOGGER_PR_TYPE type, char *stringbuf,
 	return n;
 }
 
-#else /* !(CONFIG_MTK_ENG_BUILD) || !(CONFIG_MTK_GMO_RAM_OPTIMIZE)) */
+#else /* !(CONFIG_MTK_ENG_BUILD) */
 
 unsigned int gCapturePriLayerEnable;
 unsigned int gCaptureWdmaLayerEnable;
@@ -1583,4 +1583,4 @@ void init_log_buffer(void)
 {
 }
 
-#endif /* CONFIG_MTK_ENG_BUILD) || !(CONFIG_MTK_GMO_RAM_OPTIMIZE) */
+#endif /* CONFIG_MTK_ENG_BUILD) */
